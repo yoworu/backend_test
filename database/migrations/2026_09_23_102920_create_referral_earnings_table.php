fd92bc1d6ddd90601200a6f2fe4bb810fa9a9aec
Egor <yesowo@mail.ru>
2026-09-23 17:34:05 +0700

    feat(migrations): create referral earnings table

---
+++ database/migrations/2026_09_23_102920_create_referral_earnings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('referral_earnings', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('referrer_master_id')->constrained('masters');
+            $table->foreignId('referred_master_id')->constrained('masters');
+
+            $table->foreignId('referral_id')->constrained('referrals');
+            $table->foreignId('payment_id')->constrained('payments');
+
+            $table->unsignedBigInteger('payment_amount');
+            $table->unsignedBigInteger('amount');
+            $table->unsignedTinyInteger('percent');
+
+            $table->string('status');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('referral_earnings');
+    }
+};
